db89741bc8602f64def59943331e5e6b016497c9
Sergey Zenchenko <zen@111min.com>
2012-11-10 18:09:14 +0200

    Initial Commit

---
+++ DXAppTemplateTests/DXAppTemplateTests.h
@@ -0,0 +1,13 @@
+//
+//  DXAppTemplateTests.h
+//  DXAppTemplateTests
+//
+//  Created by zen on 11/10/12.
+//  Copyright (c) 2012 111Minutes. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DXAppTemplateTests : SenTestCase
+
+@end
---
+++ DXAppTemplateTests/DXAppTemplateTests.m
@@ -0,0 +1,32 @@
+//
+//  DXAppTemplateTests.m
+//  DXAppTemplateTests
+//
+//  Created by zen on 11/10/12.
+//  Copyright (c) 2012 111Minutes. All rights reserved.
+//
+
+#import "DXAppTemplateTests.h"
+
+@implementation DXAppTemplateTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DXAppTemplateTests");
+}
+
+@end
